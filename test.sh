063671c57c8816cd11b06a7aca89e26d4d158f08
Victor Barba Martin <victor.barba@wallapop.com>
2018-08-09 20:33:34 +0200

    production yml

--- test.sh
+++ test.sh
@@ -1,15 +1,19 @@
 #!/bin/bash -e
 
 TEST_USER='{"id":1,"username":"test1","email":"test@test.com"}'
+HOST="127.0.0.1"
+PORT="5000"
+#HOST="devop-LoadB-1907IATCPWFXO-2120024973.eu-west-1.elb.amazonaws.com"
+#PORT="80"
 
 # create user
-curl -f -s -o /dev/null -H "Content-Type: application/json" -X POST -d ${TEST_USER} http://127.0.0.1:5000/user && echo "TEST CREATE USER OK" || echo "ERROR ON CREATE USER"
+curl -f -s -o /dev/null -H "Content-Type: application/json" -X POST -d ${TEST_USER} http://${HOST}:${PORT}/user && echo "TEST CREATE USER OK" || echo "ERROR ON CREATE USER"
 
 # get user
-curl -f -s -o /dev/null http://127.0.0.1:5000/user/1 && echo "TEST GET USER OK" || echo "ERROR ON GET USER"
+curl -f -s -o /dev/null http://${HOST}:${PORT}/user/1 && echo "TEST GET USER OK" || echo "ERROR ON GET USER"
 
 # get users
-curl -f -s -o /dev/null http://127.0.0.1:5000/users && echo "TEST GET USERS OK"|| echo "ERROR ON GET USERS"
+curl -f -s -o /dev/null http://${HOST}:${PORT}/users && echo "TEST GET USERS OK"|| echo "ERROR ON GET USERS"
 
 # delete user
-curl -f -s -o /dev/null -X DELETE http://127.0.0.1:5000/user/1 && echo "TEST DELETE USER OK" || echo "ERROR ON DELETE USER"
+curl -f -s -o /dev/null -X DELETE http://${HOST}:${PORT}/user/1 && echo "TEST DELETE USER OK" || echo "ERROR ON DELETE USER"
